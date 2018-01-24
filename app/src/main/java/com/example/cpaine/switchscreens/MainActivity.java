73bce6f0e6ce13bf22d55d8adbc3f12857435ddd
Chris Paine <cdpaine2003@gmail.com>
2018-01-23 21:10:08 -0500

    Initial

--- app/src/main/java/com/example/cpaine/switchscreens/MainActivity.java
+++ app/src/main/java/com/example/cpaine/switchscreens/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.cpaine.switchscreens;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onGetNameClick(View view) {
+
+        Intent getNameScreenIntent = new Intent(this,
+                SecondScreen.class);
+
+        final int result = 1;
+
+        getNameScreenIntent.putExtra("callingActivity", "MainActivity");
+        startActivityForResult(getNameScreenIntent, result);
+
     }
 }
---
+++ app/src/main/java/com/example/cpaine/switchscreens/SecondScreen.java
@@ -0,0 +1,17 @@
+package com.example.cpaine.switchscreens;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Cpaine on 1/21/2018.
+ */
+
+class SecondScreen extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
